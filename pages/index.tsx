0f2482485772c4189eb0bc2faa600d55cb735ca7
juntendou <juntendou@DESKTOP-UEFCQSF.localdomain>
2022-09-30 12:32:06 +0900

    typescript error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import header from '../components/header'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
